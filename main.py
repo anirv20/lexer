9f59c522db20ad782b240c6bfc571ae36efd7839
Anton Irvold <anirv20@student.sdu.dk>
2022-08-31 11:11:12 +0000

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+#
+# T-603-THYD Compilers
+# Project: Test driver for lexer
+#
+from lexer import SyntaxErrorException
+import lexer
+
+filename = 'test2.py'
+with open(filename) as f:
+    lex = lexer.Lexer(f)
+    token = lex.next()
+    while token.type != lexer.Tokentype.EOI:
+        print(token.type, token.lexeme if token.type != lexer.Tokentype.Newline else "\\n", token.location.line)
+        try:
+            token = lex.next()
+        except SyntaxErrorException as error:
+            print(error)
---
+++ test.py
@@ -0,0 +1,17 @@
+print("h")
+print("h")
+# T-603-THYD Compilers
+# Project: Test driver for lexer
+#
+print("He\\\"llo")
+def hello():
+    a = 1+2
+    if 1 == 2:
+      print(1+2)
+      print(1)
+      print(111)
+    elif 1 != 2:
+      print("Hello")
+print("Hello")
+print("Hello")
+print(1)
