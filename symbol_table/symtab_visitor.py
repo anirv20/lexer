347e3ac8a6e3e00136413974938e6358854922ff
Anton Irvold <anirv20@student.sdu.dk>
2022-10-11 18:44:40 +0000

    what's wrong

--- symbol_table/symtab_visitor.py
+++ symbol_table/symtab_visitor.py
@@ -178,9 +178,10 @@ class SymbolTableVisitor(visitor.Visitor):
         st = symbol_table.Class(node.name.name)
         parent = self.curr_sym_table
         self.curr_sym_table.add_child(st)
-        self.curr_sym_table = st
 
         self.do_visit(node.name)
+        self.curr_sym_table = st
+
         self.do_visit(node.super_class)
         for d in node.declarations:
             self.do_visit(d)
@@ -195,9 +196,11 @@ class SymbolTableVisitor(visitor.Visitor):
             st = symbol_table.Function(node.name.name)
         parent = self.curr_sym_table
         self.curr_sym_table.add_child(st)
-        self.curr_sym_table = st
 
         self.do_visit(node.name)
+        self.curr_sym_table = st
+
+
         for p in node.params:
             self.do_visit(p)
         self.do_visit(node.return_type)
